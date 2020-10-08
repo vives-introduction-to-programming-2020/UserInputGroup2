c81a0b306432bfc08913c9793c080297105d768f
Nico De Witte <nico.dewitte@vives.be>
2020-10-08 16:50:44 +0200

    Add code

---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Getting User Input
+
+Demo code of chapter 6 - Getting User Input.
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/UserInputGroup2.git
+```
---
+++ UserInputGroup2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UserInputGroup2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Wow, my name is also " + name);
+
+            Console.Write("Please enter your age: ");
+            string userInput = Console.ReadLine();
+            int age = Convert.ToInt32(userInput);
+
+            Console.WriteLine(age + " Damn that is almost old.");
+
+            Console.Write("Please enter your height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(height + " is quite normal");
+
+            // Random values
+            Random generator = new Random();
+
+            int randomNumber = generator.Next();
+            Console.WriteLine(randomNumber);
+
+            int dieThrow = generator.Next(1, 7);
+            Console.WriteLine(dieThrow);
+
+            int secretNumber = generator.Next(20);
+            Console.WriteLine(secretNumber);
+
+        }
+    }
+}
